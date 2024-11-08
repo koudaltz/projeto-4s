9a4c78bde7a4420aaa686ef6a4d48af6e5dee797
Daniel brito ferreira <97321854+koudaltz@users.noreply.github.com>
2024-11-07 22:40:51 -0300

    Update conexao.php

--- database/conexao.php
+++ database/conexao.php
@@ -1,9 +1,16 @@
 <?php
 
-// define('HOST', 'projetosm4.mysql.database.azure.com');
-// define('USUARIO', 'koudaltz');
-// define('SENHA', 'Daniel123@');
-// define('DB', 'gerenciamento_frota');
+define('HOST', 'projetosm4.mysql.database.azure.com');
+define('USUARIO', 'koudaltz');
+define('SENHA', 'Daniel123@');
+define('DB', 'gerenciamento_frota');
 
-// $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Conexão falhou!');
-// ?>
+$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB);
+
+if (!$conexao) {
+    die('Falha na conexão: ' . mysqli_connect_error());
+}
+
+echo 'Conexão bem-sucedida!';
+
+?>
