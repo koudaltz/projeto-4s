5e270ea8802bd26dbebf1bc55291e4a4f48b3519
Daniel brito ferreira <97321854+koudaltz@users.noreply.github.com>
2024-11-07 23:20:11 -0300

    Update conexao.php

--- database/conexao.php
+++ database/conexao.php
@@ -1,13 +1,13 @@
-// <?php
+<?php
 
-// define('HOST', 'projetosm4.mysql.database.azure.com');
-// define('USUARIO', 'koudaltz');
-// define('SENHA', 'Daniel123@');
-// define('DB', 'gerenciamento_frota');
-// define('PORT', 3306);
+define('HOST', 'projetosm4.mysql.database.azure.com');
+define('USUARIO', 'koudaltz');
+define('SENHA', 'Daniel123@');
+define('DB', 'gerenciamento_frota');
+define('PORT', 3306);
 
-// echo 'Conexão bem-sucedida!';
+echo 'Conexão bem-sucedida!';
 
-// mysqli_close($conexao);
+mysqli_close($conexao);
 
-// ?>
+?>
