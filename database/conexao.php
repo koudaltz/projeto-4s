27f554f9f0b29d833758c8f562cd0dacac45045e
Daniel brito ferreira <97321854+koudaltz@users.noreply.github.com>
2024-11-07 23:23:21 -0300

    Update conexao.php

--- database/conexao.php
+++ database/conexao.php
@@ -6,7 +6,7 @@ define('SENHA', 'Daniel123@');
 define('DB', 'gerenciamento_frota');
 define('PORT', 3306);
 
-echo 'Conexão bem-sucedida!';
+// echo 'Conexão bem-sucedida!';
 
 mysqli_close($conexao);
 
