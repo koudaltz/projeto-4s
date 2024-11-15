9f8b9e777eab79763dfe6602f34bd712c8e89de7
Daniel brito ferreira <97321854+koudaltz@users.noreply.github.com>
2024-11-14 22:46:04 -0300

    atualizando conexão

--- database/conexao.php
+++ database/conexao.php
@@ -1,9 +1,22 @@
 <?php
 
-define('HOST', '127.0.0.1');
-define('USUARIO', 'adtra');
-define('SENHA', 'adtra');
+define('HOST', 'projetosm4.mysql.database.azure.com');
+define('USUARIO', 'koudaltz');
+define('SENHA', 'Daniel@1234');
 define('DB', 'gerenciamento_frota');
+define('PORT', 3306);
 
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Conexão falhou!');
+$conexao = mysqli_init();
+
+mysqli_ssl_set($conexao, NULL, NULL, NULL, NULL, NULL); // Configura SSL
+mysqli_real_connect($conexao, HOST, USUARIO, SENHA, DB, PORT, NULL, MYSQLI_CLIENT_SSL);
+
+//if (mysqli_connect_errno()) {
+//    die('Conexão falhou: ' . mysqli_connect_error());
+//} else {
+//    echo 'Conexão bem-sucedida!';
+//}
+
+//mysqli_close($conexao);
+
 ?>
